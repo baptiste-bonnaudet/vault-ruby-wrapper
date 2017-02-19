b5095f78f646e71e6ce9b5f084a132d9428f02e2
Baptiste Bonnaudet <baptiste.bonnaudet@C2QRK041GM13.local>
2017-02-19 14:53:31 -0500

    first commit

---
+++ lib/vault-ruby-wrapper.rb
@@ -0,0 +1,35 @@
+require 'vault'
+
+module Vault
+  VALID_NAME = /^[\.\-[:alnum:]_]+$/
+
+  def self.validate_name!(name)
+    unless name =~ VALID_NAME
+      raise "DataBags must have a name matching #{VALID_NAME.inspect}, you gave #{name.inspect}"
+    end
+  end
+
+  def self.secret_data(secret)
+    mysecret = nil
+    Vault.with_retries(Vault::HTTPConnectionError, attempts: 5) do
+        mysecret = Vault.logical.read("secret/#{secret}") # rubocop:disable Lint/AssignmentInCondition
+    end
+    return mysecret.data if mysecret
+    raise "Secret '#{secret}' returned an empty value"
+  end
+
+  def self.data_bag(secret)
+    validate_name!(secret)
+    return secret_data(secret).keys
+  end
+  def self.data_bag_item(secret, item)
+    validate_name!(secret)
+    validate_name!(item)
+
+    i = secret_data(secret)[item.to_sym]
+    unless i.to_s.strip.empty?
+      return i
+    end
+    raise 'item does not exist'
+  end
+end
---
+++ vault-ruby-wrapper.gemspec
@@ -0,0 +1,14 @@
+Gem::Specification.new do |s|
+  s.name        = 'vault-ruby-wrapper'
+  s.version     = '0.1.0'
+  s.date        = '2017-02-16'
+  s.summary     = "Hola!"
+  s.description = "A simple hello world gem"
+  s.authors     = ["Baptiste Bonnaudet"]
+  s.email       = 'baptiste.bonnaudet@lightpseedhq.com'
+  s.files       = ["lib/vault-ruby-wrapper.rb"]
+  s.add_runtime_dependency 'vault', '~> 0.7.0'
+  s.homepage    =
+    'http://rubygems.org/gems/hola'
+  s.license       = 'MIT'
+end
